c72085c3c67d38825ed875b59b9aa71174bdf954
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-06-07 15:48:05 +0100

    Add Animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,7 +16,7 @@ function type() {
 		clearInterval(interval);
 		setTimeout(function() {
 			interval = setInterval(Delete, 100);
-		}, 1000);
+		}, 3000);
 	}
 }
 
@@ -57,7 +57,36 @@ function scrollFunction() {
     document.getElementById("head").style.height = "60px";
     document.getElementById("head").style.boxShadow = "0 4px 8px 0 rgba(80,113,105,0.2)";
     document.getElementById("nav").style.marginTop = "10px";
-     
+    var elem = document.getElementsByClassName("whi");
+    for (let i = 0; i < elem.length; i++) {
+        elem[i].style.color="#000";
+    }
+
+    document.getElementById('abo').onmouseover = function(e) {
+        e.style.color = '#008cba';
+    }
+
+    let top  = window.pageYOffset + window.innerHeight;
+    let isVisible = top > document.querySelector('.skills').offsetTop;
+    let isVisible1 = top > document.querySelector('.gallery').offsetTop;
+    let isVisible2 = top > document.querySelector('#cont').offsetTop;
+
+
+    if (isVisible) {
+        document.getElementById('abo').style.color = '#008CBA'
+    }
+
+    if (isVisible1) {
+        document.getElementById('project').style.color = '#008CBA'
+        document.getElementById('abo').style.color = '#000'
+
+    }
+
+    if (isVisible2) {
+        document.getElementById('contact').style.color = '#008CBA'
+        document.getElementById('project').style.color = '#000'
+    }
+
   } else {
     document.getElementById("head").style.backgroundColor = "rgba(0, 0, 0, 0)";
     document.getElementById("head").style.height = "80px";
@@ -69,3 +98,75 @@ function scrollFunction() {
     }
   }
 }
+
+document.addEventListener('scroll', function (e) {
+    let top  = window.pageYOffset + window.innerHeight;
+    let isVisible = top > document.querySelector('#ab-cont').offsetTop;
+    let isVisible1 = top > document.querySelector('#skill1').offsetTop;
+    let isVisible2 = top > document.querySelector('#skill2').offsetTop;
+    let isVisible3 = top > document.querySelector('#skill3').offsetTop;
+    let isVisible4 = top > document.querySelector('#skill4').offsetTop;
+    let isVisible5 = top > document.querySelector('#prof').offsetTop;
+    let isVisible6 = top > document.querySelector('#fea').offsetTop;
+    let isVisible7 = top > document.querySelector('#fil').offsetTop;
+    let isVisible8 = top > document.querySelector('.pro').offsetTop;
+    let isVisible9 = top > document.querySelector('#get').offsetTop;
+    let isVisible10 = top > document.querySelector('#cont').offsetTop;
+    let isVisible11 = top > document.querySelector('.detail').offsetTop;
+   
+
+  
+    if (isVisible) {
+       document.getElementById('ab-cont').classList.add('animate');
+    }
+
+    if (isVisible1) {
+        document.getElementById('skill1').classList.add('animate');
+    }
+
+    if (isVisible2) {
+        document.getElementById('skill2').classList.add('animate');
+    }
+
+    if (isVisible3) {
+        document.getElementById('skill3').classList.add('animate');
+    }
+
+    if (isVisible4) {
+        document.getElementById('skill4').classList.add('animate');
+    }
+
+    if (isVisible5) {
+        document.getElementById('prof').classList.add('animate');
+    }
+
+    if (isVisible6) {
+        document.getElementById('fea').classList.add('animate');
+    }
+
+    if (isVisible7) {
+        document.getElementById('fil').classList.add('animate');
+    }
+
+    if (isVisible8) {
+        let elem = document.querySelectorAll('.pro');
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].classList.add('animate');
+        }
+    }
+
+    if (isVisible9) {
+        document.getElementById('get').classList.add('animate');
+    }
+
+    if (isVisible10) {
+        document.getElementById('cont').classList.add('animate');
+    }
+
+    if (isVisible11) {
+        let elem = document.querySelectorAll('.detail');
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].classList.add('animate');
+        }
+    }
+  });
